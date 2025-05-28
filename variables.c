a8a4ef1f754d3f631acef7966757d057772be99b
edersonsm <edersondesouzamarques@gmail.com>
2025-05-28 19:24:13 -0300

    variables

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int emp_num = 7;
+    float salary;
+    char grade;
+    double balance_amount;
+
+    const float pi = 3.14;
+
+    printf("\f Print function!");
+
+    return 0;
+}
